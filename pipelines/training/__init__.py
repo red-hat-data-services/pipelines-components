6bea62e23a21aa7ac3e423d213cacb11233283b4
kramaranya <kramaranya15@gmail.com>
2026-01-13 11:43:38 +0000

    chore: remove eager imports from finetuning component
    Signed-off-by: kramaranya <kramaranya15@gmail.com>

--- pipelines/training/__init__.py
+++ pipelines/training/__init__.py
@@ -4,7 +4,5 @@ This module re-exports all pipelines in the training category for easy import:
     from kfp_components.pipelines.training import pipeline_name
 """
 
-from .osft import osft_pipeline
-from .sft import sft_pipeline
-
-__all__ = ["sft_pipeline", "osft_pipeline"]
+# Auto-generated imports will be added here by scripts/update_init_imports.py
+# Pipelines will be imported dynamically based on subdirectories
