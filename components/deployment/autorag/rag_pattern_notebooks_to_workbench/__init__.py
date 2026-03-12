26e5bdfd92f91cf0da1ca4174c02d0829f5fcf5b
Lukasz Cmielowski <lcmielow@redhat.com>
2026-03-12 10:10:32 +0100

    remove the component for promoting notebooks to workbench
    Signed-off-by: Lukasz Cmielowski <lcmielow@redhat.com>

--- components/deployment/autorag/rag_pattern_notebooks_to_workbench/__init__.py
+++
@@ -1,3 +0,0 @@
-from .component import rag_pattern_notebooks_to_workbench
-
-__all__ = ["rag_pattern_notebooks_to_workbench"]
--- pipelines/training/autorag/documents_lite_rag_optimization_pipeline/tests/test_pipeline_unit.py
+++ pipelines/training/autorag/documents_lite_rag_optimization_pipeline/tests/test_pipeline_unit.py
@@ -13,9 +13,7 @@ class TestDocumentsRagOptimizationPipelineUnitTests:
 
     def test_pipeline_with_default_parameters(self):
         """Test pipeline has expected interface (required args)."""
-        inputs = getattr(
-            documents_lite_rag_optimization_pipeline, "_component_inputs", set()
-        )
+        inputs = getattr(documents_lite_rag_optimization_pipeline, "_component_inputs", set())
         assert "test_data_secret_name" in inputs
         assert "test_data_bucket_name" in inputs
         assert "chat_model_url" in inputs
